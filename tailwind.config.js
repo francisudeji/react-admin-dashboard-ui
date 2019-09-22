d45b33ba28d464e48eba2d241e45af7cd03fc500
francisudeji <francis.udejiofficial@gmail.com>
2019-09-22 19:16:12 +0100

    Tailwind extensions

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,29 @@
 module.exports = {
   theme: {
-    extend: {}
+    extend: {
+      colors: {
+        'light-gray': '#F7F6F3',
+        'link-green': '#529C4E',
+        'link-blue': '#637086',
+        'heading-blue': '#6B778C',
+        'user-gray': '#D8D8D8',
+        'badge-red': '#DE3618',
+        'badge-gray': '#F7D3CC',
+        'dark-blue': '#41516C',
+        'dark-blue-name': '#485771',
+        'dark-blue-number': '#475772',
+        'darker-blue': '#172B4D',
+        'light-blue': '#4A5A74',
+        'list-blue': '#445470',
+        'list-gray': '#F9F9FA',
+        'small-number': '#939BAA',
+        'chevron-blue': '#8F98A8'
+      },
+      spacing: {
+        '64.5': '16.5rem',
+        '25': '7rem'
+      }
+    }
   },
   variants: {},
   plugins: []
