05e8af961c4b0d4ce4e90d8779d57f30af3d8497
francisudeji <francis.udejiofficial@gmail.com>
2019-09-23 07:34:12 +0100

    Subcomponents to layout components

--- src/components/main.js
+++ src/components/main.js
@@ -1,12 +1,59 @@
 import React from 'react'
+import { FaBell } from 'react-icons/fa'
 
-function Main() {
+function Main({ children, name }) {
   return (
-    <main className='main'>
-      <header className='main__header'>
-        <h1>Balances</h1>
-      </header>
-      <div className='main__body'></div>
+    <main className='main flex-1 py-4'>
+      <div className='w-11/12 lg:w-4/5 mx-auto'>
+        <header className='flex flex-col lg:flex-row lg:justify-between lg:items-center'>
+          <div className='flex items-center'>
+            <button className='text-darker-blue border border-darker-blue p-2 rounded mr-4 hover:text-white hover:bg-darker-blue focus:text-white focus:bg-darker-blue lg:hidden'>
+              <svg
+                className='fill-current h-4 w-4'
+                viewBox='0 0 20 20'
+                xmlns='http://www.w3.org/2000/svg'
+              >
+                <title>Menu</title>
+                <path d='M0 3h20v2H0V3zm0 6h20v2H0V9zm0 6h20v2H0v-2z' />
+              </svg>
+            </button>
+            <h1 className='text-3xl font-bold m-0 p-0 text-darker-blue lg:text-4xl'>
+              {name}
+            </h1>
+          </div>
+          <div className='right flex justify-between items-center mt-4 lg:mt-0'>
+            <div className='flex items-center mr-10'>
+              <input
+                type='checkbox'
+                value='none'
+                id='mode'
+                name='check'
+                defaultChecked
+                className='mr-2'
+              />
+              <label
+                htmlFor='mode'
+                className='lg:text-lg font-bold text-dark-blue'
+              >
+                Test Mode
+              </label>
+            </div>
+            <div className='mr-10 relative'>
+              <span className='absolute right-0 top-0 -mr-1 -mt-1 bg-badge-red w-4 h-4 rounded-full text-badge-gray text-xs flex items-center justify-center text-center'>
+                1
+              </span>
+              <FaBell className='text-2xl' />
+            </div>
+            <div className='flex justify-center items-center text-dark-blue-name font-semibold'>
+              <span className='h-8 w-8 rounded-full flex items-center justify-center text-center bg-user-gray'>
+                O
+              </span>
+              <span className='ml-3 flex-1 text-right'>Oluwatobi Mayowa</span>
+            </div>
+          </div>
+        </header>
+        {children}
+      </div>
     </main>
   )
 }
--- src/components/sidebar.js
+++ src/components/sidebar.js
@@ -1,61 +1,101 @@
 import React from 'react'
-// import { Link } from 'react-router-dom'
+import { Link } from 'react-router-dom'
+import { FaChevronDown, FaCreditCard, FaCode, FaCog } from 'react-icons/fa'
 
-function Sidebar() {
+function Sidebar({ name }) {
+  const className = pathname =>
+    name.toLowerCase() === pathname
+      ? 'text-link-green font-bold'
+      : 'text-link-blue'
+
   return (
-    <aside className='sidebar'>
-      <header className='sidebar__header'>
-        <button className='btn btn--logo'>B</button>
-        <button className='btn btn--dropdown'>Business name</button>
+    <aside
+      className='absolute top-0 left-0 h-100vh -ml-64.5 sidebar w-64.5 border-r-2 border-gray-300 p-4 lg:block lg:relative lg:ml-0'
+      style={{ transition: 'all 0.5s ease-in-out' }}
+    >
+      <header className='flex pt-3'>
+        <button className='w-8 h-8 bg-white shadow rounded text-sm font-semibold'>
+          B
+        </button>
+        <button className='h-8 px-1 ml-1 bg-transparent text-lg font-bold text-darker-blue flex items-center'>
+          Business name{' '}
+          <FaChevronDown className='ml-2 mt-1 text-xs text-chevron-blue' />
+        </button>
       </header>
-      <nav className='sidebar__nav'>
-        <ul className='list'>
-          <li className='list__item'>
-            <a href='/'>
+      <nav className='mt-10 ml-2'>
+        <ul>
+          <li className='mt-1'>
+            <a
+              className='py-2 text-link-blue font-normal flex items-center'
+              href='/'
+            >
               <span>
-                <img src='/assets/svgs/home.svg' alt='Dashboard' />
+                <img
+                  className='h-4 w-4 fill-current'
+                  src='/assets/svgs/home.svg'
+                  alt=''
+                />
               </span>
-              Dashboard
+              <span className='ml-3'>Dashboard</span>
             </a>
           </li>
-          <li className='list__item'>
-            <a href='/'>
+          <li className='mt-1'>
+            <Link
+              className={`py-2 flex items-center ${className('balances')}`}
+              to='/'
+            >
               <span>
-                <img src='/assets/svgs/balances.svg' alt='Balances' />
+                <img
+                  className='h-4 w-4 fill-current'
+                  src='/assets/svgs/balances.svg'
+                  alt=''
+                />
               </span>
-              Balances
+              <span className='ml-3'>Balances</span>
+            </Link>
+          </li>
+          <li className='mt-1'>
+            <a
+              className='py-2 text-link-blue flex items-center font-normal'
+              href='/'
+            >
+              <span>
+                <FaCreditCard />
+              </span>
+              <span className='ml-3'>Payments</span>
             </a>
           </li>
-          <li className='list__item'>
-            <a href='/'>
+          <li className='mt-1'>
+            <Link
+              className={`py-2 flex items-center ${className('transfers')}`}
+              to='/transfers'
+            >
               <span>
-                <img src='/assets/svgs/home.svg' alt='Payments' />
+                <img className='h-4 w-4' src='/assets/svgs/home.svg' alt='' />
               </span>
-              Payments
+              <span className='ml-3'>Transfers</span>
+            </Link>
+          </li>
+          <li className='mt-1'>
+            <a
+              className='py-2 text-link-blue flex items-center font-normal'
+              href='/'
+            >
+              <span>
+                <FaCode />
+              </span>
+              <span className='ml-3'>Developers</span>
             </a>
           </li>
-          <li className='list__item'>
-            <a href='/'>
+          <li className='mt-1'>
+            <a
+              className='py-2 text-link-blue flex items-center font-normal'
+              href='/'
+            >
               <span>
-                <img src='/assets/svgs/home.svg' alt='Transfers' />
+                <FaCog />
               </span>
-              Transfers
-            </a>
-          </li>
-          <li className='list__item'>
-            <a href='/'>
-              <span>
-                <img src='/assets/svgs/home.svg' alt='Developers' />
-              </span>
-              Developers
-            </a>
-          </li>
-          <li className='list__item'>
-            <a href='/'>
-              <span>
-                <img src='/assets/svgs/home.svg' alt='Settings' />
-              </span>
-              Settings
+              <span className='ml-3'>Settings</span>
             </a>
           </li>
         </ul>
